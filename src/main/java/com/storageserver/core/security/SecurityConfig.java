c6f003dfe4cc413dc794905227072c29498e1c31
alegrarsiogiftalesmana <alegrarsio2005@gmail.com>
2025-10-14 11:43:45 +0700

    add security filter chain system

---
+++ src/main/java/com/storageserver/core/security/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.storageserver.core.security;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${api.auth.key-name}")
+    private String apiKeyHeaderName;
+
+    @Value("${api.auth.key-value}")
+    private String apiKeyValue;
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .addFilterBefore(new ApiKeyAuthFilter(apiKeyHeaderName, apiKeyValue), UsernamePasswordAuthenticationFilter.class)
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().authenticated() 
+            );
+        return http.build();
+    }
+}
